94da35fb4640dfc26fe2bd0f5c3072ead0926031
Yole <igbavagodswill4@gmail.com>
2025-04-25 19:48:04 +0100

    Building the static version of the home page, currently done with the hero section of that page

--- src/components/Circlebtn.js
+++ src/components/Circlebtn.js
@@ -1,11 +1,13 @@
 export default function CircleButton({
   children,
   background = "#0755e9",
-  border = "solid 2px #ffffff",
+  border = "solid 4px #ffffff",
   width = 90.39,
   height = 89.46,
   color = "#ffffff",
+  fontSize = 14,
   borderRadius = 100,
+  className = "",
 }) {
   const customBtnStyle = {
     display: "flex",
@@ -15,9 +17,14 @@ export default function CircleButton({
     height: `${height}px`,
     background,
     color,
+    fontSize: `${fontSize}px`,
     border,
     borderRadius: `${borderRadius}%`,
   };
 
-  return <div style={customBtnStyle}>{children}</div>;
+  return (
+    <div style={customBtnStyle} className={className}>
+      {children}
+    </div>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,92 @@
+import CircleButton from "../components/Circlebtn";
+import heroImage from "../assets/home/hero_image.png";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="page-container">
+      <HeroSection />
+      <ServicesSection />
+    </div>
+  );
+}
+
+function HeroSection() {
+  return (
+    <section className="hero-section">
+      <p className="hero-heading">Choose Bloom as your web design agency</p>
+      <p className="hero-text">
+        Provide top quality service with unlimited revisions, dedicated to
+        helping businesses thrive through captivating and effective visuals.
+      </p>
+
+      <div className="hero-image-container">
+        <CircleButton fontSize={26} className="hero-img-circle">
+          <ion-icon name="arrow-down-outline"></ion-icon>
+        </CircleButton>
+        <img src={heroImage} alt="Hero" className="hero-image" />
+      </div>
+
+      <div className="know-more-section">
+        <p className="know-more-heading">
+          Provide the best service, with ideas that are out of the box
+        </p>
+
+        <div className="know-more-content">
+          <p className="know-more-text">
+            At Boom Agency we’re passionate about customer experience and team
+            culture. When you chose to join Bloom Agency you are joining a team
+            that genuinely loves what they do.
+          </p>
+
+          <button className="know-more-btn">
+            Know more
+            <span>
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </span>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function ServicesSection() {
+  return (
+    <section className="service-container">
+      <div className="service-section">
+        <div className="service-descriptions">
+          <p className="service-heading">We offer several service for you</p>
+
+          <p className="service-text">
+            Bloom is your one-stop web design agency, offering a full range of
+            services to elevate your brand, enhance your online presence, and
+            drive business growth.
+          </p>
+        </div>
+
+        <ServiceList />
+      </div>
+    </section>
+  );
+}
+
+function ServiceList() {
+  const serviceList = [
+    "Web Design",
+    "Web Development",
+    "App Design",
+    "Branding Design",
+    "UI/UX",
+    "Ecommerce",
+    "Full Stack Development",
+  ];
+  return (
+    <div className="service-list">
+      {serviceList.map((service, i) => (
+        <div className="service" key={i}>
+          {service}
+        </div>
+      ))}
+    </div>
+  );
 }
