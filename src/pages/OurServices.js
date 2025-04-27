5dfa471cec463cd867b7d5b85a000fdfb5327261
Yole <igbavagodswill4@gmail.com>
2025-04-27 14:06:54 +0100

    done creating the desktop view of the services page

--- src/pages/OurServices.js
+++ src/pages/OurServices.js
@@ -48,6 +48,7 @@ export default function OurServices() {
       <HeadingSection />
       <OurServiceSection />
       <FaqSection data={faqs} />
+      <RequestForCallSection />
     </div>
   );
 }
@@ -240,5 +241,27 @@ function Faq({ children, questionNumber, question, onOpen, curOpen }) {
         ></ion-icon>
       </div>
     </div>
+  );
+}
+
+function RequestForCallSection() {
+  return (
+    <section className="request-for-call-section">
+      <div className="request-for-call-container">
+        <p>Offering The Best Experience Of Digital Branding Services</p>
+
+        <button className="request-btn">
+          Request A Free Call
+          <CircleButton
+            background="#ff6016"
+            width={46.07}
+            height={46.07}
+            fontSize={24}
+          >
+            <ion-icon name="call"></ion-icon>
+          </CircleButton>
+        </button>
+      </div>
+    </section>
   );
 }
