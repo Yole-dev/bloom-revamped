5b5b38fff1a5be1baaf914013dfc8d822e957d07
Yole <igbavagodswill4@gmail.com>
2025-05-02 20:46:03 +0100

    added the calendar functionality to the request for a call section and currently able to update state based on the date picked

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,13 +1,58 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function BookFreeCallForm() {
   return (
     <form action="">
-      <CustomDatePicker />
+      <CustomCalendarContainer>
+        <CustomDatePicker />
+      </CustomCalendarContainer>
     </form>
   );
 }
 
-function CustomDatePicker() {
-  return;
+function CustomCalendarContainer({ className, children }) {
+  return (
+    <div className="calendar-wrapper">
+      <div className="custom-calendar-container">
+        <div className="calendar-header-text">
+          <p>Select a Day</p>
+        </div>
+
+        <div className="calendar-header">
+          <p>calendar</p>
+          <p>
+            <span>{new Date().getFullYear()}</span> <br />
+            {`${new Date().toLocaleString("en-US", {
+              weekday: "short",
+              month: "long",
+              day: "numeric",
+            })}`}
+          </p>
+        </div>
+
+        <div className="calendar-body">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+function CustomDatePicker({ className }) {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const maxDate = new Date();
+  maxDate.setMonth(maxDate.getMonth() + 6);
+
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      dateFormat="MMMM d, yyyy h:mm aa"
+      minDate={new Date()}
+      maxDate={maxDate}
+      inline
+      className="react-datepicker"
+    />
+  );
 }
