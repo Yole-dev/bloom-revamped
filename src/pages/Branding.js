f3656c349c6ce0d9777996f85d42aba6917a1219
Yole <igbavagodswill4@gmail.com>
2025-05-05 21:01:48 +0100

    done styling the first main section moving on to the next section

--- src/pages/Branding.js
+++ src/pages/Branding.js
@@ -26,6 +26,9 @@ export default function Branding() {
     <PageAnimation className="branding-main-container">
       <div className="branding-sub-container">
         <HeaderSection />
+        <MainSection>
+          <BrandingGoals />
+        </MainSection>
       </div>
 
       <div className="branding-sub-container">
@@ -55,6 +58,79 @@ function HeaderSection() {
           <ion-icon name="arrow-down-outline"></ion-icon>
         </CircleButton>
       </div>
+
+      <p>
+        Branding power lies in fostering trust with unacquainted companies.
+        Contemplate its implications for your business. The accuracy of branding
+        is crucial, and our guidance ensures optimal results.
+      </p>
     </ComponentAnimation>
+  );
+}
+
+function MainSection({ children }) {
+  return <section className="main-section-container">{children}</section>;
+}
+
+function BrandingGoals() {
+  return (
+    <section className="branding-goals">
+      <p>Whatever your branding goals are, Bloom will help you succeed.</p>
+
+      <LeftComponentAnimation className="branding-goals-content">
+        <p>Brand identity design for a new business</p>
+        <p>
+          First impressions last longer. If you are going to work on your
+          branding, you might as well do it right. As a new business, BLOOM will
+          help with your brand messaging, logo, collateral and guidelines. We
+          can also help with the design of your website.
+        </p>
+      </LeftComponentAnimation>
+
+      <RightComponentAnimation className="branding-goals-content">
+        <p>Rebranding an existing business</p>
+        <p>
+          Is there a new direction for your company? Or is your brand identity
+          now ancient and uninspiring? If yes, it’s time to refresh your brand.
+          Rebranding is a big (and sometimes complicated) move, but we will help
+          you navigate this seamlessly.
+        </p>
+      </RightComponentAnimation>
+
+      <LeftComponentAnimation className="branding-goals-content">
+        <p>Design of additional brand stationary and assets.</p>
+        <p>
+          Every element of your brand should work together to create the right
+          experience. Let’s work together create additional brand assets such as
+          business cards, letterheads, packaging, social media banners, email
+          templates, and much more.
+        </p>
+      </LeftComponentAnimation>
+
+      <RightComponentAnimation className="branding-goals-content">
+        <p>Brand strategy and guideline development</p>
+        <p>
+          Your brand goes beyond logo and colours. It involves your messaging,
+          your voice, your tone, and your culture. It also needs to be
+          consistent. Through clearly defined brand guidelines, we will help you
+          ensure that your brand is used the right way – every time.
+        </p>
+      </RightComponentAnimation>
+
+      <Button
+        width={300}
+        height={103}
+        borderRadius={54}
+        background="#FF6016"
+        fontSize={31}
+        fontWeight={400}
+        className="lets-talk-btn"
+      >
+        Let's Talk
+        <span className="lets-talk-icon">
+          <ion-icon name="arrow-forward-outline"></ion-icon>
+        </span>
+      </Button>
+    </section>
   );
 }
