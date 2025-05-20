e9a43521423dbcb43a5a0b127ec3ec3aedceb2a5
Yole <igbavagodswill4@gmail.com>
2025-05-20 13:14:16 +0100

    Changed file path case on the logo images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
@@ -13,98 +14,116 @@ import Ecommerce from "./pages/Ecommerce";
 import RequestACall from "./pages/RequestACall";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(function () {
+    // timing the page loader
+    const timer = setTimeout(() => setIsLoading(false), 3000);
+
+    //cleaning up the effect
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Home />
-              </Layout>
-            }
-          />
-          <Route
-            path="/ourservices"
-            element={
-              <Layout>
-                <OurServices />
-              </Layout>
-            }
-          />
-          <Route
-            path="/portfolio"
-            element={
-              <Layout>
-                <Portfolio />
-              </Layout>
-            }
-          />
-          <Route
-            path="/aboutus"
-            element={
-              <Layout>
-                <AboutUs />
-              </Layout>
-            }
-          />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              path="/ourservices"
+              element={
+                <Layout>
+                  <OurServices />
+                </Layout>
+              }
+            />
+            <Route
+              path="/portfolio"
+              element={
+                <Layout>
+                  <Portfolio />
+                </Layout>
+              }
+            />
+            <Route
+              path="/aboutus"
+              element={
+                <Layout>
+                  <AboutUs />
+                </Layout>
+              }
+            />
 
-          <Route
-            path="/contactus"
-            element={
-              <Layout>
-                <ContactUs />
-              </Layout>
-            }
-          />
+            <Route
+              path="/contactus"
+              element={
+                <Layout>
+                  <ContactUs />
+                </Layout>
+              }
+            />
 
-          <Route
-            path="/branding"
-            element={
-              <Layout>
-                <Branding />
-              </Layout>
-            }
-          />
+            <Route
+              path="/branding"
+              element={
+                <Layout>
+                  <Branding />
+                </Layout>
+              }
+            />
 
-          <Route
-            path="/webdevelopment"
-            element={
-              <Layout>
-                <WebDevelopment />
-              </Layout>
-            }
-          />
+            <Route
+              path="/webdevelopment"
+              element={
+                <Layout>
+                  <WebDevelopment />
+                </Layout>
+              }
+            />
 
-          <Route
-            path="/ui_ux"
-            element={
-              <Layout>
-                <UiUx />
-              </Layout>
-            }
-          />
+            <Route
+              path="/ui_ux"
+              element={
+                <Layout>
+                  <UiUx />
+                </Layout>
+              }
+            />
 
-          <Route
-            path="/ecommerce"
-            element={
-              <Layout>
-                <Ecommerce />
-              </Layout>
-            }
-          />
+            <Route
+              path="/ecommerce"
+              element={
+                <Layout>
+                  <Ecommerce />
+                </Layout>
+              }
+            />
 
-          <Route
-            path="/requestacall"
-            element={
-              <Layout>
-                <RequestACall />
-              </Layout>
-            }
-          />
-        </Routes>
-      </Router>
+            <Route
+              path="/requestacall"
+              element={
+                <Layout>
+                  <RequestACall />
+                </Layout>
+              }
+            />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
+}
+
+function Loading() {
+  return <p>Loading...</p>;
 }
